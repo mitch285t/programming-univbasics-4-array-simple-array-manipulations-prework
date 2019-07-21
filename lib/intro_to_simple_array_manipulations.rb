b14ba0f05dc8deb8989463217a4f178fe963f2a6
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-21 20:37:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,7 +22,7 @@ def using_concat(array,array2)
 array.concat(array2)
 end 
 def using_insert(array, element)
-array.insert(element)
+array.insert(4, element)
 end 
 def using_uniq(array)
 return array.uniq 
